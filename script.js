e46c7f1d3eadc5029f3f8245a57ea21aee4f7121
Abhishek Rawat <abhishekrawat5743@gmail.com>
2019-11-22 11:26:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function showTab(n) {
   }
   if (n == (x.length - 1)) {
     document.getElementById("nextBtn").innerHTML = "Submit";
-    document.getElementById("nextBtn").onclick="window.location.href = 'https://rohitth69.github.io/Register/confirm';
+//     document.getElementById("nextBtn").onclick="window.location.href = 'https://rohitth69.github.io/Register/confirm';
   } else {
     document.getElementById("nextBtn").innerHTML = "Next";
   }
