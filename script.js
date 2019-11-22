75692c7c751a40179c3c1696e2d19caf9a05a266
Abhishek Rawat <abhishekrawat5743@gmail.com>
2019-11-22 09:52:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function showTab(n) {
   }
   if (n == (x.length - 1)) {
     document.getElementById("nextBtn").innerHTML = "Submit";
+    document.getElementById("nextBtn").href = "https://rohitth69.github.io/Register/confirm.html";
   } else {
     document.getElementById("nextBtn").innerHTML = "Next";
   }
